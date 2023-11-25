c9e28d2ada0a8ce4bca7cfd599851c6f8c720f88
bopaki <btebalo@gmail.com>
2023-11-25 03:06:04 +0000

    Update carousel images and text color

--- app/page.tsx
+++ app/page.tsx
@@ -9,25 +9,25 @@ export default class NextJsCarousel extends Component {
 		return ( 
 			<div className="h-screen flex items-center justify-center bg-gray-100"> 
 			<h2></h2>
-      <Carousel animationHandler="slide"  showIndicators showThumbs={false} showArrows={false} autoPlay swipeable interval={3000}  infiniteLoop>
+      <Carousel animationHandler="slide"  showIndicators showThumbs={false} showArrows={false} autoPlay swipeable interval={8000} transitionTime={500} infiniteLoop>
 				<div className="relative"> 
-					<img src="/assets/books.jpg" alt="image1" className=""/>
-          <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-5xl font-serif">Career Advice</h1>
+					<img src="/assets/graduation-group.jpg" alt="image1" className="h-screen"/>
+          <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-black text-5xl font-serif">Career Advice</h1>
 				</div> 
 				<div className="relative"> 
-					<img src="/assets/music.jpg" alt="image2" className=""/> 
+					<img src="/assets/music.jpg" alt="image2" className="h-screen"/> 
           <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-5xl font-serif">About Us</h1>
 				</div> 
 				<div className="relative"> 
-					<img src="/assets/ocean.jpg" alt="image3" className=""/> 
+					<img src="/assets/ocean.jpg" alt="image3" className="h-screen"/> 
           <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-5xl font-serif">Sample CV</h1>
 				</div> 
 				<div className="relative"> 
-					<img src="/assets/student.jpg" alt="image4" className=""/> 
+					<img src="/assets/student.jpg" alt="image4" className="h-screen"/> 
           <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-5xl font-serif">Sponsors</h1>
 				</div> 
 				<div className="relative"> 
-					<img src="/assets/student-group.jpg" alt="image5" className=""/> 
+					<img src="/assets/student-group.jpg" alt="image5" className="h-screen"/> 
           <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-5xl font-serif">Diversity</h1>
 				</div> 
 			</Carousel> 
