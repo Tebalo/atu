c94b021096a8b9943bc8f1cd17311f0c309b368d
bopaki <btebalo@gmail.com>
2023-11-25 02:45:43 +0000

    Update carousel images and add custom styles

--- app/page.tsx
+++ app/page.tsx
@@ -2,36 +2,33 @@
 import React, { Component } from 'react'; 
 import "react-responsive-carousel/lib/styles/carousel.min.css"; 
 import { Carousel } from 'react-responsive-carousel'; 
+import './CustomCarousel.css';
 
 export default class NextJsCarousel extends Component { 
 	render() { 
 		return ( 
 			<div className="h-screen flex items-center justify-center bg-gray-100"> 
 			<h2></h2>
-      <Carousel autoPlay swipeable transitionTime={300} infiniteLoop emulateTouch>
+      <Carousel animationHandler="slide"  showIndicators showThumbs={false} showArrows={false} autoPlay swipeable interval={3000}  infiniteLoop>
 				<div className="relative"> 
-					<img src="/assets/books.jpg" alt="image1"/> 
-					<p className="absolute bottom-0 left-0 bg-black bg-opacity-50 text-white p-2">Image 1</p> 
-
+					<img src="/assets/books.jpg" alt="image1" className=""/>
+          <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-5xl font-serif">Career Advice</h1>
 				</div> 
 				<div className="relative"> 
-					<img src="/assets/music.jpg" alt="image2" /> 
-					<p className="absolute bottom-0 left-0 bg-black bg-opacity-50 text-white p-2">Image 2</p> 
-
+					<img src="/assets/music.jpg" alt="image2" className=""/> 
+          <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-5xl font-serif">About Us</h1>
 				</div> 
 				<div className="relative"> 
-					<img src="/assets/ocean.jpg" alt="image3"/> 
-					<p className="absolute bottom-0 left-0 bg-black bg-opacity-50 text-white p-2">Image 3</p> 
-
+					<img src="/assets/ocean.jpg" alt="image3" className=""/> 
+          <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-5xl font-serif">Sample CV</h1>
 				</div> 
 				<div className="relative"> 
-					<img src="/assets/student.jpg" alt="image4"/> 
-					<p className="absolute bottom-0 left-0 bg-black bg-opacity-50 text-white p-2">Image 4</p> 
-
+					<img src="/assets/student.jpg" alt="image4" className=""/> 
+          <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-5xl font-serif">Sponsors</h1>
 				</div> 
 				<div className="relative"> 
-					<img src="/assets/student-group.jpg" alt="image5" /> 
-					<p className="absolute bottom-0 left-0 bg-black bg-opacity-50 text-white p-2">Image 5</p> 
+					<img src="/assets/student-group.jpg" alt="image5" className=""/> 
+          <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-5xl font-serif">Diversity</h1>
 				</div> 
 			</Carousel> 
 			</div> 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,11 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        'sans': ['Helvetica', 'Arial', 'sans-serif'],
+        'serif': ['Times New Roman', 'serif'],
+        'mono': ['Courier New', 'monospace'],
+      }
     },
   },
   plugins: [],
